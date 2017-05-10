93127ecc5892442b1f8e606b8118de50b4bd955c
David Espinoza <despinoza1027@gmail.com>
2017-05-10 14:35:12 -0500

    set up dotenv, db route now local

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27020/data')
-
+mongoose.connect(process.env.DB_PATH)
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
